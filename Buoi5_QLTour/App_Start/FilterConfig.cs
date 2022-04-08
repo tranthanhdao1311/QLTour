d197b99aec13f0ee4dc8889a65eb03c095cab2d6
tranthanhdao1311 <tranthanhdao1311@gmail.com>
2022-04-08 13:29:33 +0700

    hello

---
+++ Buoi5_QLTour/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Buoi5_QLTour
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Buoi5_QLTour/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Buoi5_QLTour.MvcApplication" Language="C#" %>
